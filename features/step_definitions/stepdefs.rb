f33dbed97fe907c44c1796e6b181a1035aeae491
Jose Pettoruti <jepettoruti@gmail.com>
2018-11-20 17:33:17 +0000

    Refactoring using outlines and variables

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,34 +1,17 @@
-# Given("today is Sunday") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
-
-# When("I ask whether it's Friday yet") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
-
-# Then("I should be told {string}") do |string|
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
-
-
 module FridayStepHelper
   def is_it_friday(day)
     if day == 'Friday'
       'TGIF'
     else
       'Nope'
-    end
+    end  
   end
 end
 
 World FridayStepHelper
 
-Given("today is Sunday") do
-  @today = 'Sunday'
-end
-
-Given("today is Friday") do
-  @today = 'Friday'
+Given("today is {string}") do |given_day|
+  @today = given_day
 end
 
 When("I ask whether it's Friday yet") do
